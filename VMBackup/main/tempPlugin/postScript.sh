1d9c11766f93d7d093900a42a4095212c4fa154f
root <root@VMOracleLinux7.cw1yt0fngn3e3jref5ef1koj4d.ix.internal.cloudapp.net>
2016-10-14 07:30:57 -0400

    scripts updated

--- VMBackup/main/tempPlugin/postScript.sh
+++ VMBackup/main/tempPlugin/postScript.sh
@@ -1,7 +1,25 @@
-#!/bin/bash
-param1=$1
-param2=$2
+nstance=$1
 
-printf  "$param1 \n">>"/etc/scriptLog"
-printf  "$param2 \n">>"/etc/scriptLog"
+# variables used for returning the status of the scripts
+success=0
+error=1
+warning=2
+
+retVal=$success
+
+log_path="/etc/postScript.log"   #path of log file
+printf  "Instance: $instance \n" > $log_path
+
+csession $instance -U%SYS "##Class(Backup.General).ExternalThaw()"
+status=$?
+if [ $status -eq 5 ]; then
+echo "SYSTEM IS UNFROZEN"
+printf  "SYSTEM IS UNFROZEN\n" >> $log_path
+elif [ $status -eq 3 ]; then
+echo "SYSTEM UNFREEZE FAILED"
+printf  "SYSTEM UNFREEZE FAILED\n" >> $log_path
+retVal=$error
+fi
+
+exit $retVal
 
--- VMBackup/main/tempPlugin/preScript.sh
+++ VMBackup/main/tempPlugin/preScript.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
-param1=$1
-param2=$2
+instance=$1
 
-printf  "$param1 \n">"/etc/scriptLog"
-printf  "$param2 \n">>"/etc/scriptLog"
+# variables used for returning the status of the scripts
+success=0
+error=1
+warning=2
+
+retVal=$success
+
+log_path="/etc/preScript.log"   #path of log file
+printf  "Instance: $instance \n" > $log_path
+
+csession $instance -U%SYS "##Class(Backup.General).ExternalFreeze()"
+status=$?
+if [ $status -eq 5 ]; then
+echo "SYSTEM IS FROZEN"
+printf  "SYSTEM IS FROZEN\n" >> $log_path
+elif [ $status -eq 3 ]; then
+echo "SYSTEM FREEZE FAILED"
+printf  "SYSTEM FREEZE FAILED\n" >> $log_path
+retVal=$error
+csession $instance -U%SYS "##Class(Backup.General).ExternalThaw()"
+fi
+
+exit $retVal
 
